546a4abaf13f45a74145ddc02f17e9c6eea7bc1e
Ramanan Sivaranjan <ramanan@funkaoshi.com>
2023-12-05 23:50:35 -0500

    Clean up via ruff/black.

--- 3/day3a.py
+++ 3/day3a.py
@@ -1,13 +1,12 @@
-from collections import defaultdict
-from typing import List, Tuple
 import sys
+from typing import List
 
 
-def check_adjacent_symbols(engine: List[str], x: int, y: int) -> List[Tuple[int, int]]:
+def check_adjacent_symbols(engine: List[str], x: int, y: int) -> bool:
     # look for gears around the current position
     above = y - 1 >= 0 and engine[y - 1][x] not in ".0123456789"
-    below = y + 1 < len(engine) and engine[y + 1][x]  not in ".0123456789"
-    current = engine[y][x] not in  ".0123456789"
+    below = y + 1 < len(engine) and engine[y + 1][x] not in ".0123456789"
+    current = engine[y][x] not in ".0123456789"
 
     return above or below or current
 
@@ -15,7 +14,7 @@ def check_adjacent_symbols(engine: List[str], x: int, y: int) -> List[Tuple[int,
 filename = "input.txt" if not len(sys.argv) > 1 else sys.argv[1]
 
 with open(filename) as f:
-    engine = [l.replace("\n", ".") for l in f.readlines()]
+    engine = [line.replace("\n", ".") for line in f.readlines()]
 
 # the list of valid parts we have found
 parts = []
@@ -32,29 +31,29 @@ for y in range(len(engine)):
             next_char_is_digit = engine[y][x + 1] in "0123456789"
         except IndexError:
             next_char_is_digit = False
-        
+
         if engine[y][x] in "0123456789":
             current_part += engine[y][x]
 
         if current_part or next_char_is_digit:
-            # As we read a part number we will check for adjacent symbols in the rectangle around it.
+            # As we read a part number we will check for adjacent symbols
+            # in the rectangle around it.
             valid_part = valid_part or check_adjacent_symbols(engine, x, y)
-        
+
         if engine[y][x] not in "0123456789":
-            # read a period or symbol (., *, etc). Every line ends with a '.' so we will 
-            # always reset the current_part / valid_part state variables.
+            # read a period or symbol (., *, etc). Every line ends with a '.'
+            # so we willalways reset the current_part / valid_part state variables.
 
             if current_part and valid_part:
-                # if we have read a part and it's adjacent to a symbol we store it
                 parts.append(current_part)
 
             # reset the valid part and current part values
             current_part = ""
 
             if next_char_is_digit:
-               # reset to just the symbols adjacent to the next part
-               # TODO: called twice when starting a new part
-               valid_part = check_adjacent_symbols(engine, x, y)
+                # reset to just the symbols adjacent to the next part
+                # TODO: called twice when starting a new part
+                valid_part = check_adjacent_symbols(engine, x, y)
             else:
                 valid_part = False
 
--- 3/day3b.py
+++ 3/day3b.py
@@ -1,6 +1,6 @@
+import sys
 from collections import defaultdict
 from typing import List, Tuple
-import sys
 
 
 def get_adjacent_gears(engine, x, y):
@@ -10,11 +10,11 @@ def get_adjacent_gears(engine, x, y):
     above = y - 1 >= 0 and engine[y - 1][x] == "*"
     if above:
         valid_gears.append((y - 1, x))
-    
+
     below = y + 1 < len(engine) and engine[y + 1][x] == "*"
     if below:
         valid_gears.append((y + 1, x))
-    
+
     current = engine[y][x] == "*"
     if current:
         valid_gears.append((y, x))
@@ -25,7 +25,7 @@ def get_adjacent_gears(engine, x, y):
 filename = "input.txt" if not len(sys.argv) > 1 else sys.argv[1]
 
 with open(filename) as f:
-    engine = [l.replace("\n", ".") for l in f.readlines()]
+    engine = [line.replace("\n", ".") for line in f.readlines()]
 
 # list of gears we have found, and the parts adjacent to them
 gears = defaultdict(list)
@@ -47,16 +47,14 @@ for y in range(len(engine)):
             current_part += engine[y][x]
 
         if current_part or next_char_is_digit:
-            # As we read a part number we will check for adjacent gears in the rectangle around it.
+            # As we read a part number we will check for adjacent gears
+            # in the rectangle around it.
             valid_gears.extend(get_adjacent_gears(engine, x, y))
 
         if engine[y][x] not in "0123456789":
-            # read a period or symbol (., *, etc). Every line ends with a '.' so we will 
-            # always reset the current_part / valid_gears state variables by the time
-            # we are done processing the line.
-
-            if current_part:
-                print(f"{y} {x} - current_part: {current_part} - valid_gears: {valid_gears}")
+            # read a period or symbol (., *, etc). Every line ends with a '.'
+            # so we will always reset the current_part / valid_gears state
+            # variables by the time we are done processing the line.
 
             if current_part and valid_gears:
                 # save the part next to any adjacent gears
