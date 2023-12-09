0e417a01ca52819315cd1f69cddc1f50182bb9ec
Ramanan Sivaranjan <ramanan@funkaoshi.com>
2023-12-09 10:33:24 -0500

    Can find previous number by reversing sequence.

--- 9/day9.py
+++ 9/day9.py
@@ -30,18 +30,6 @@ def find_next_value(sequence_differences: dict[int, list[int]]) -> int:
     return sequence_differences[0][-1]
 
 
-def find_proceeding_value(sequence_differences: dict[int, list[int]]) -> int:
-    for i in range(len(sequence_differences) - 1, 0, -1):
-        top_element = sequence_differences[i][0]
-        first_element = sequence_differences[i - 1][0]
-        print(f"{i}: {top_element=}, {first_element=}")
-        sequence_differences[i - 1].insert(0, first_element - top_element)
-
-    print(f"Proceeding value: {sequence_differences[0][0]}")
-
-    return sequence_differences[0][0]
-
-
 filename = "input.txt" if len(sys.argv) == 1 else sys.argv[1]
 
 with open(filename) as f:
@@ -52,9 +40,10 @@ sequences = [[int(n) for n in s.split()] for s in raw_sequences]
 next_values = []
 previous_values = []
 for sequence in sequences:
-    diffs = find_sequence_differences(sequence)
-    next_values.append(find_next_value(diffs))
-    previous_values.append(find_proceeding_value(diffs))
+    next_values.append(find_next_value(find_sequence_differences(sequence)))
+
+    # for part 2 of the problem we find the next number in the reversed sequence.
+    previous_values.append(find_next_value(find_sequence_differences(sequence[::-1])))
 
 print(sum(next_values))
 print(sum(previous_values))
