639b69160f2a1ec8b38c02ffb0fe66ff3e2a07c7
Ramanan Sivaranjan <ramanan@funkaoshi.com>
2023-12-12 21:03:00 -0500

    Day 12 part 1 works. Yay.

--- 12/day12.py
+++ 12/day12.py
@@ -1,7 +1,11 @@
 import argparse
+import cProfile
+import io
+import pstats
 from dataclasses import dataclass
 
 import regex as re
+from loguru import logger
 
 
 @dataclass
@@ -16,39 +20,52 @@ class ConditionRecord:
 
 # map of spring_status & list of damaged_springs -> number of arrangements
 arrangement_cache = {}
+regex_cache = {}
 
 
 def make_key(spring_status: str, damaged_springs: list[str]):
     return f"{spring_status}||{','.join(damaged_springs)}"
 
 
-def broken_spring_combinations(spring_status: str, damaged_springs: list[str], level=0):
-    print(f"{'  ' * level} {spring_status}, {damaged_springs}")
-
+def broken_spring_combinations(
+    spring_status: str, damaged_springs: list[str], current_arrangement="", level=0
+) -> list[str]:
     if not damaged_springs:
         if "#" not in spring_status:
-            # we have a valid arrangement
-            return 1
+            return [current_arrangement + spring_status.replace("?", ".")]
         else:
             # we still have bad springs to process, but our records say we don't.
-            return 0
+            return []
 
     # look for the next contiguous set of broken springs
-    regex = re.compile("[?#]{" + damaged_springs[0] + "}")
+    if damaged_springs[0] not in regex_cache:
+        regex_cache[damaged_springs[0]] = re.compile("[?#]{" + damaged_springs[0] + "}")
 
-    arrangements = 0
+    regex = regex_cache[damaged_springs[0]]
+
+    arrangements = []
     for match in regex.finditer(spring_status, overlapped=True):
-        # for each possible location that fits X broken springs, validate
-        # another broken spring is not adjacent to it.
         start, end = match.span()
+
+        # Broken springs proceed this match, so any subsequent match will be invalid.
+        if "#" in spring_status[:start]:
+            break
+
+        # Broken springs adjacent to this match.
         if end < len(spring_status) and spring_status[end] == "#":
             continue
         if start > 0 and spring_status[start - 1] == "#":
             continue
 
-        # We handle this case in the recursion call, but may as well also stop now
+        # We handle this case in the recursion call, but may as well also stop now.
+        # We have matched the whole string, but have more damaged springs to process.
         if len(spring_status[end + 1 :]) == 0 and len(damaged_springs[1:]) > 0:
             continue
+
+        next_arrangement = current_arrangement + (
+            spring_status[:start].replace("?", ".") + "#" * int(damaged_springs[0])
+        )
+        next_arrangement += "." if end < len(spring_status) else ""
 
         # we have found a spot X broken springs could fit, followed by an end
         # of line, "." or a "?". We skip that spot as it can't be a broken
@@ -56,10 +73,11 @@ def broken_spring_combinations(spring_status: str, damaged_springs: list[str], l
         key = make_key(spring_status[end + 1 :], damaged_springs[1:])
         if key not in arrangement_cache:
             arrangement_cache[key] = broken_spring_combinations(
-                spring_status[end + 1 :], damaged_springs[1:], level + 1
+                spring_status[end + 1 :],
+                damaged_springs[1:],
+                next_arrangement,
+                level + 1,
             )
-        else:
-            print(f"{'  ' * level} - Cache hit for {key} = {arrangement_cache[key]}")
 
         arrangements += arrangement_cache[key]
 
@@ -69,9 +87,11 @@ def broken_spring_combinations(spring_status: str, damaged_springs: list[str], l
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--filename", action="store", default="input.txt")
+    parser.add_argument("-c", "--copies", action="store", default=0, type=int)
     args = parser.parse_args()
 
     filename = args.filename
+    copies = args.copies
 
     with open(filename) as f:
         file = f.read().splitlines()
@@ -80,16 +100,29 @@ if __name__ == "__main__":
     for row in file:
         spring_status, damaged_springs = row.split()
         damaged_springs_list = damaged_springs.split(",")
+        if copies > 0:
+            spring_status = "?".join([spring_status] * copies)
+            damaged_springs_list = damaged_springs_list * copies
         condition_records.append(ConditionRecord(spring_status, damaged_springs_list))
+
+    pr = cProfile.Profile()
+    pr.enable()
 
     total_arrangements = []
     for cr in condition_records:
-        print(f"Find solutions for {cr.spring_status} and {cr.damaged_springs}")
-        arrangement = broken_spring_combinations(
+        arrangements = broken_spring_combinations(
             cr.spring_status, cr.damaged_springs, level=1
         )
-        print(f"-> Arrangements: {arrangement}")
-        print("---")
-        total_arrangements.append(arrangement)
+        total_arrangements.append(len(arrangements))
 
-    print(sum(total_arrangements))
+        logger.info(f"Found {len(arrangements)} arrangements for {cr.spring_status}")
+        logger.info(f"Processed {len(total_arrangements)} of {len(condition_records)}")
+
+    pr.disable()
+    s = io.StringIO()
+    sortby = "cumulative"
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps.print_stats()
+    print(s.getvalue())
+
+logger.info(f"Total: {sum(total_arrangements)}")
